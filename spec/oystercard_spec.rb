4710e30548addbbbf215553145381e86b0b3489c
olwend <olwend@gmail.com>
2017-01-09 13:16:11 +0000

    04_first_test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require './lib/Oystercard'
+
 describe Oystercard do
-  
+
+it 'talks' do
+  is_expected.to respond_to :balance
+end
+it 'has a starting balance of zero' do
+  expect(subject.balance).to eq(0)
+end
 end
