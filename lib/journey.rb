bb9a74ef4c6619c27aa2bf4c5e71844ce4592ae7
olwend <olwend@gmail.com>
2017-01-10 18:11:58 +0000

    14.journey_end set

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,8 +1,24 @@
 class Journey
+
   def initialize start_station
+    @start_station = start_station
   end
 
   def in_journey?
     true
   end
+
+  def start_station
+    @start_station
+  end
+
+  def end_station
+    @end_station
+  end
+
+  def end station
+  @end_station = station
+    #deduct BALANCE_MIN
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,8 +1,18 @@
 require "journey"
+
 describe Journey do
   let(:station) {double :Kings, name: "Kings",zone: 1}
+  let(:end_station) {double :Pimlico, name: "Pimlico",zone: 1}
   subject {described_class.new(station)}
 
   it{is_expected.to be_in_journey} ##
 
+  it'has a start station set' do
+    expect(subject.start_station).to eq station
+  end
+
+  it'has an end station set' do
+    subject.end end_station
+    expect(subject.end_station).to eq end_station
+  end
 end
